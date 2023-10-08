5b6598ce5be15b60a363fc72e4d165c80ac87c53
Loukas Vovlas <vovlasloukas@gmail.com>
2023-10-08 14:21:19 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        locations: resolve(__dirname, 'locations.html'),
+        about: resolve(__dirname, 'about.html'),
+        appDesign: resolve(__dirname, 'app-design.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        graphicDesign: resolve(__dirname, 'graphic-design.html'),
+        webDesign: resolve(__dirname, 'web-design.html'),
+      },
+    },
+  },
+})
