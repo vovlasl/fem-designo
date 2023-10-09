fe352212d9e3034f3164bf339bd6f379d887b082
Loukas Vovlas <vovlasloukas@gmail.com>
2023-10-09 21:59:44 +0200

    navigation toggle update

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const navigation = document.querySelector(".navigation");
 const iconMenu = document.querySelector(".icon-hamburger");
 const iconMenuClose = document.querySelector(".icon-close");
-
+const body = document.querySelector("body");
+const navToggleStyle = window.getComputedStyle(navToggle);
 
 navToggle.addEventListener('click', () => {
     navigation.hasAttribute("data-visible")
@@ -14,5 +15,25 @@ navToggle.addEventListener('click', () => {
     iconMenu.setAttribute("style", iconMenuClose.getAttribute("style"));
     iconMenuClose.setAttribute("style", temp);
 
+    if (iconMenuClose.getAttribute("style") == "display: block;") {
+        body.setAttribute("style", "overflow-y: hidden");
+    } else {
+        body.setAttribute("style", "overflow-y: visible");
+    }
+
     navigation.toggleAttribute("data-visible");
 });
+
+
+window.displayListener = function () {
+    if (navToggleStyle.getPropertyValue("display") == "none") {
+        body.setAttribute("style", "overflow-y: visible;");
+    } else {
+        if (navigation.hasAttribute("data-visible")) {
+            window.scrollTo(0, 0);
+            body.setAttribute("style", "overflow-y: hidden");
+        } else {
+            body.setAttribute("style", "overflow-y: visible");
+        }
+    }
+}
